11079226254d6e81bb2c995c522b8e6ca9c8d09a
Niels Gandraß <niels@gandrass.de>
2023-04-23 18:00:09 +0200

    Clone repository during post_install since subrepo is not initialized properly by iocage. Also introduce patches.

--- post_install.sh
+++ post_install.sh
@@ -3,8 +3,15 @@ set -e
 
 # Clone truenas-spindown-timer
 SPINDOWN_TIMER_VERSION=2.2.0
-git clone https://github.com/ngandrass/truenas-spindown-timer.git /opt/truenas-spindown-timer
-git -C /opt/truenas-spindown-timer checkout tags/v${SPINDOWN_TIMER_VERSION}
+SPINDOWN_TIMER_DIR=/opt/truenas-spindown-timer
+
+git config --global advice.detachedHead false
+git config --global user.email "root@localhost"
+git config --global user.name "root"
+
+git clone https://github.com/ngandrass/truenas-spindown-timer.git "$SPINDOWN_TIMER_DIR"
+git -C "$SPINDOWN_TIMER_DIR" checkout tags/v${SPINDOWN_TIMER_VERSION}
+find /opt/truernas-spindown-timer-patches -name "*.patch" -exec git -C "$SPINDOWN_TIMER_DIR" apply {} \;
 
 # Setup service
 chmod u+x /usr/local/etc/rc.d/spindown_timer
